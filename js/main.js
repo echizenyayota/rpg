da05efb20fef5b45d3a42dcc49f7418051ef53bf
echizenyayota <virtualmisso30@gmail.com>
2019-01-14 12:17:59 +0900

    #10 データ型について見ていこう

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+console.log(typeof 'hello');
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof function(x){ return x*x; } );
