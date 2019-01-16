9ed2a89872d3fd340f1fad75366d7d1f83684b0e
echizenyayota <virtualmisso30@gmail.com>
2019-01-16 14:37:16 +0900

    main.jsの初期化

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 'use strict';
+{
 
-console.log(typeof 'hello');
-console.log(typeof 5);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
-console.log(typeof function(x){ return x*x; } );
+
+
+}
