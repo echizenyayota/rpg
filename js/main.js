b23080041ee26618b8659f4c9224d78aec825dfa
echizenyayota <virtualmisso30@gmail.com>
2019-01-16 18:01:20 +0900

    正解の表示

--- js/main.js
+++ js/main.js
@@ -16,20 +16,19 @@
     const hero = new DragonFantasy('Tom', '勇者', '100');
     console.log(hero.show());
 
-    // class DragonFantasyMp extends DragonFantasy {
-    //     constructor(name, job, hp, mp){ // Bobの職業は戦士。現在のMPは0です。
-    //         super(name, job, hp);
-    //         this.mp = mp;
-    //     }
-    //
-    //     showMp() {
-    //         return `${this.name}の職業は${this.job}。現在のMPは${this.mp}です。`;
-    //     }
-    // }
-    //
+    class DragonFantasyMp extends DragonFantasy {
+        constructor(name, job, hp, mp){ // Bobの職業は戦士。現在のMPは0です。
+            super(name, job, hp);
+            this.mp = mp;
+        }
 
-    // const fighter = new DragonFantasyMp('Bob', '戦士', '200', '0');
-    // console.log(fighter.showMp());
+        showMp() {
+            return `${this.name}の職業は${this.job}。現在のMPは${this.mp}です。`;
+        }
+    }
+
+    const fighter = new DragonFantasyMp('Bob', '戦士', '200', '0');
+    console.log(fighter.showMp());
 
     // class DragonFantasyMp extends DragonFantasy {
     //     constructor(mp){ // Bobの職業は戦士。現在のMPは0です。
@@ -45,21 +44,21 @@
     // const fighter = new DragonFantasyMp(0);
     // console.log(fighter.showMp());
 
-    class DragonFantasyMp extends DragonFantasy {
-        constructor(mp){ // undefinedの職業はundefined。現在のMPはBobです(ダメな例。子クラスが親クラスを上書きしている)
-            super();
-            this.mp = mp;
-        }
-
-        showMp() {
-            // 子クラスが上書きしているためthis.nameとthis.jobが定義されていない状態
-            return `${this.name}の職業は${this.job}。現在のMPは${this.mp}です。`;
-        }
-    }
-
-    // this.mpがDragonFantasyのthis.nameを上書きしているため'Bob'しか引数を取っていない
-    const fighter = new DragonFantasyMp('Bob', '戦士', 200, 0);
-    console.log(fighter.showMp());
+    // class DragonFantasyMp extends DragonFantasy {
+    //     constructor(mp){ // undefinedの職業はundefined。現在のMPはBobです(ダメな例。子クラスが親クラスを上書きしている)
+    //         super();
+    //         this.mp = mp;
+    //     }
+    //
+    //     showMp() {
+    //         // 子クラスが上書きしているためthis.nameとthis.jobが定義されていない状態
+    //         return `${this.name}の職業は${this.job}。現在のMPは${this.mp}です。`;
+    //     }
+    // }
+    //
+    // // this.mpがDragonFantasyのthis.nameを上書きしているため'Bob'しか引数を取っていない
+    // const fighter = new DragonFantasyMp('Bob', '戦士', 200, 0);
+    // console.log(fighter.showMp());
 
 
 }
