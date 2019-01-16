dd65b835706fc27f87272147efba27bae931619c
echizenyayota <virtualmisso30@gmail.com>
2019-01-16 17:34:56 +0900

    別解

--- js/main.js
+++ js/main.js
@@ -16,9 +16,23 @@
     const hero = new DragonFantasy('Tom', '勇者', '100');
     console.log(hero.show());
 
+    // class DragonFantasyMp extends DragonFantasy {
+    //     constructor(name, job, hp, mp){ // Bobの職業は戦士。現在のMPは0です。
+    //         super(name, job, hp);
+    //         this.mp = mp;
+    //     }
+    //
+    //     showMp() {
+    //         return `${this.name}の職業は${this.job}。現在のMPは${this.mp}です。`;
+    //     }
+    // }
+    //
+    // const fighter = new DragonFantasyMp('Bob', '戦士', '200', '0');
+    // console.log(fighter.showMp());
+
     class DragonFantasyMp extends DragonFantasy {
-        constructor(name, job, hp, mp){ // Bobの職業は戦士。現在のMPは0です。
-            super(name, job, hp);
+        constructor(mp){ // Bobの職業は戦士。現在のMPは0です。
+            super('Bob', '戦士', 200);
             this.mp = mp;
         }
 
@@ -27,7 +41,7 @@
         }
     }
 
-    const fighter = new DragonFantasyMp('Bob', '戦士', '200', '0');
+    const fighter = new DragonFantasyMp(0);
     console.log(fighter.showMp());
 
 
