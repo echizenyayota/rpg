3e00404fcee434f79907b8a6bc2491e4d98d331b
echizenyayota <virtualmisso30@gmail.com>
2019-01-16 18:00:05 +0900

    ダメな例の作成

--- js/main.js
+++ js/main.js
@@ -31,18 +31,34 @@
     // const fighter = new DragonFantasyMp('Bob', '戦士', '200', '0');
     // console.log(fighter.showMp());
 
+    // class DragonFantasyMp extends DragonFantasy {
+    //     constructor(mp){ // Bobの職業は戦士。現在のMPは0です。
+    //         super('Bob', '戦士', 200);
+    //         this.mp = mp;
+    //     }
+    //
+    //     showMp() {
+    //         return `${this.name}の職業は${this.job}。現在のMPは${this.mp}です。`;
+    //     }
+    // }
+    //
+    // const fighter = new DragonFantasyMp(0);
+    // console.log(fighter.showMp());
+
     class DragonFantasyMp extends DragonFantasy {
-        constructor(mp){ // Bobの職業は戦士。現在のMPは0です。
-            super('Bob', '戦士', 200);
+        constructor(mp){ // undefinedの職業はundefined。現在のMPはBobです(ダメな例。子クラスが親クラスを上書きしている)
+            super();
             this.mp = mp;
         }
 
         showMp() {
+            // 子クラスが上書きしているためthis.nameとthis.jobが定義されていない状態
             return `${this.name}の職業は${this.job}。現在のMPは${this.mp}です。`;
         }
     }
 
-    const fighter = new DragonFantasyMp(0);
+    // this.mpがDragonFantasyのthis.nameを上書きしているため'Bob'しか引数を取っていない
+    const fighter = new DragonFantasyMp('Bob', '戦士', 200, 0);
     console.log(fighter.showMp());
 
 
